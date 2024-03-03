a409ad3717f9725b00198526a159a5c74bf8fda4
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-03-03 21:22:22 +0200

    Added main function

--- js/index.js
+++ js/index.js
@@ -2,21 +2,23 @@ const play = document.getElementById('play');
 const choices = ['green', 'red', 'yellow', 'blue'];
 const randomChoice = choices[Math.floor(Math.random() * choices.length)]; 
 const tiles = document.querySelectorAll('.tile');
-const active = document.querySelector('.unclickable')
+const active = document.querySelector('.unclickable');
 let pattern = [];
 let userPattern = [];
-let counter = 0
-
+let counter = 0;
+let clicksCounter = 0;
 
 play.addEventListener('click', function(){
     active.classList.remove('unclickable');
-    generatePattern()
+    generatePattern();
 }
 )
 
 function generatePattern(){
     pattern.push(randomChoice);
     console.log(randomChoice);
+    clicksCounter = 0;
+    
     //CHANGE TO LIGHT ON TILE
 }
 
@@ -25,19 +27,48 @@ function saveUserPattern(){
         const tile = tiles[i];
         tile.addEventListener('click', function(){
             userPattern.push(tile.dataset.tile);
-            console.log('userPattern: '+userPattern)
-            counter += 1;
-            console.log('counter: ' + counter)
-            console.log('pattern length: ' + pattern.length)
-            if (counter == pattern.length){
+            console.log(userPattern);
+            clicksCounter += 1;
+            console.log(clicksCounter);
+            console.log(pattern.length);
+            if (clicksCounter == pattern.length){
+                console.log('if');
                 active.classList.add('unclickable');
+                main();
             }
-    })
+        })
+    }   
+}
+saveUserPattern()
+
+//active.classList.remove('unclickable');
+function main(){
+    console.log('main');
+    console.log(pattern)
+    console.log(userPattern)
+    if(userPattern == pattern){
+        console.log('ho');
+        counter += 1;
+        generatePattern();
+        
+
+        if(counter == 12){
+            alert('Congrats! You won at Simon says');
+        }
+        else{
+            alert('Oh oh! you missed');
+            //RESET THE GAME
+        }
     }
     
+
 }
 
-saveUserPattern()
+
+
+
+
+
 
 
 
