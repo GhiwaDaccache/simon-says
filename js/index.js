713e224b029b26e69ed384211319039878144d8b
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-03-03 20:07:41 +0200

    Limited user clicks

--- js/index.js
+++ js/index.js
@@ -2,28 +2,42 @@ const play = document.getElementById('play');
 const choices = ['green', 'red', 'yellow', 'blue'];
 const randomChoice = choices[Math.floor(Math.random() * choices.length)]; 
 const tiles = document.querySelectorAll('.tile');
+const active = document.querySelector('.unclickable')
 let pattern = [];
 let userPattern = [];
 let counter = 0
 
 
 play.addEventListener('click', function(){
-    const active = document.querySelector('.unclickable');
     active.classList.remove('unclickable');
+    generatePattern()
 }
 )
 
 function generatePattern(){
     pattern.push(randomChoice);
-    console.log(randomChoice)
+    console.log(randomChoice);
+    //CHANGE TO LIGHT ON TILE
 }
-generatePattern()
 
-for(let i=0; i<choices.length; i++ ){
-    const tile = tiles[i]
-    tile.addEventListener('click', function(){
-        userPattern.push(tile.dataset.tile)
-        console.log(tile.dataset.tile)
-        console.log(userPattern)
-})
+function saveUserPattern(){
+    for(let i=0; i<choices.length; i++ ){
+        const tile = tiles[i];
+        tile.addEventListener('click', function(){
+            userPattern.push(tile.dataset.tile);
+            console.log('userPattern: '+userPattern)
+            counter += 1;
+            console.log('counter: ' + counter)
+            console.log('pattern length: ' + pattern.length)
+            if (counter == pattern.length){
+                active.classList.add('unclickable');
+            }
+    })
+    }
+    
 }
+
+saveUserPattern()
+
+
+
