c35d172d07ed9ffd7db159f206bad25f79d1c253
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-03-03 19:27:21 +0200

    Saved user choice

--- js/index.js
+++ js/index.js
@@ -1,8 +1,29 @@
 const play = document.getElementById('play');
+const choices = ['green', 'red', 'yellow', 'blue'];
+const randomChoice = choices[Math.floor(Math.random() * choices.length)]; 
+const tiles = document.querySelectorAll('.tile');
+let pattern = [];
+let userPattern = [];
+let counter = 0
+
 
 play.addEventListener('click', function(){
-    console.log('1')
     const active = document.querySelector('.unclickable');
     active.classList.remove('unclickable');
 }
 )
+
+function generatePattern(){
+    pattern.push(randomChoice);
+    console.log(randomChoice)
+}
+generatePattern()
+
+for(let i=0; i<choices.length; i++ ){
+    const tile = tiles[i]
+    tile.addEventListener('click', function(){
+        userPattern.push(tile.dataset.tile)
+        console.log(tile.dataset.tile)
+        console.log(userPattern)
+})
+}
